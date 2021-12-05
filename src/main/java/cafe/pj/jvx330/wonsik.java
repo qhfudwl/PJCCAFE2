1aaacc32f4abb9da897b2d1b123f0a2c82d1f73b
jos06 <jos06@DESKTOP-OOIMKMA>
2021-12-05 16:32:21 +0900

    4rd

--- src/main/java/cafe/pj/jvx330/wonsik.java
+++ src/main/java/cafe/pj/jvx330/wonsik.java
@@ -4,8 +4,8 @@ public class wonsik {
 
 	public static void main(String[] args) {
 		System.out.println("hello!");
-		System.out.println("두번 쨰 수정입니다.");
-		System.out.println("세번 째 수정입니다.");
+		System.out.println("google!");
+		System.out.println("naver!");
 
 	}
 
