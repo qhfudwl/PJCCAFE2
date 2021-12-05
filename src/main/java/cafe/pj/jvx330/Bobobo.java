c2e21fbbb84c2cffaf9bda937709ff3464e59c26
Owner <Owner@PC-2004EOV>
2021-12-05 18:51:25 +0900

    12312321313

--- src/main/java/cafe/pj/jvx330/Bobobo.java
+++ src/main/java/cafe/pj/jvx330/Bobobo.java
@@ -4,5 +4,6 @@ public class Bobobo {
 	public static void main(String[] args) {
 		System.out.println("수정된 bobobo");
 		System.out.println("수정된 bobobo123123");
+		System.out.println("수정된 bobobo123123456");
 	}
 }
