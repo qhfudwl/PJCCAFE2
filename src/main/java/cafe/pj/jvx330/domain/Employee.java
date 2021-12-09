4ff2ff2c262d17b0f4c68dfb36d740387ae98a46
Administrator <Administrator@DESKTOP-FU2AS1G>
2021-12-09 12:38:50 +0900

    update User -> delete userType update pom.xml -> add json udpate order.jsp, sales/controller/Order.java

--- src/main/java/cafe/pj/jvx330/domain/Employee.java
+++ src/main/java/cafe/pj/jvx330/domain/Employee.java
@@ -10,15 +10,15 @@ public class Employee extends User {
 	public Employee() {
 		super();
 	}        
-	public Employee(char userType, String eid, String passwd, String position) {
-		super(userType);
+	public Employee( String eid, String passwd, String position) {
+
 		this.eid = eid;
 		this.passwd = passwd;
 		this.position = position;
 	}
 	
-	public Employee(long id, char userType, String eid, String passwd, String position, Date regDate) {
-		super(id, userType, regDate);
+	public Employee(long id, String eid, String passwd, String position, Date regDate) {
+
 		this.eid = eid;
 		this.passwd = passwd;
 		this.position = position;
--- src/main/java/cafe/pj/jvx330/domain/User.java
+++ src/main/java/cafe/pj/jvx330/domain/User.java
@@ -4,20 +4,16 @@ import java.util.Date;
 
 public abstract class User {
 	protected long id;
-	protected char userType;
+
 	protected Date regDate;
 	
 	public User() {
 		
 	}
 	
-	public User(char userType) {
-		this.userType = userType; 
-	}
-	
-	public User(long id, char userType, Date regDate) {
+
+	public User(long id, Date regDate) {
 		this.id = id;
-		this.userType = userType;
 		this.regDate = regDate;
 	}
 	
@@ -33,11 +29,5 @@ public abstract class User {
 	public void setRegDate(Date regDate) {
 		this.regDate = regDate;
 	}
-	public char getUserType() {
-		return userType;
-	}
-	public void setUserType(char userType) {
-		this.userType = userType;
-	}
-	
+
 }
