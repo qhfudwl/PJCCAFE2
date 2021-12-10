1cd0355595c00fa53940c2129bfe4e5b70f60dc9
jos06 <jos06@DESKTOP-OOIMKMA>
2021-12-10 19:01:05 +0900

    CustomerCommand revise and add UserController

---
+++ src/main/java/cafe/pj/jvx330/user/controller/UserReviseController.java
@@ -0,0 +1,8 @@
+package cafe.pj.jvx330.user.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller("user.controller.userReviseController")
+public class UserReviseController {
+	
+}
